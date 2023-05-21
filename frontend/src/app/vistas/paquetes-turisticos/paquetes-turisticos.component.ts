f70d7231feeab058917db148d72f74a4735311fe
Pablo Ballatore <pfb1983@outlook.com>
2023-05-21 20:09:04 -0300

    mi parte de frontend

--- frontend/src/app/vistas/paquetes-turisticos/paquetes-turisticos.component.ts
+++ frontend/src/app/vistas/paquetes-turisticos/paquetes-turisticos.component.ts
@@ -10,6 +10,7 @@ export class PaquetesTuristicosComponent {
 
   data: any[] = [];
   data2: any[] = [];
+  dataSofia: any[] = [];
 
 
   constructor (private apiService: ApiService) {}
@@ -17,6 +18,7 @@ export class PaquetesTuristicosComponent {
   ngOnInit():void{
     this.llenarData()
     this.llenarData2()
+    this.llenarDataSofia()
   }
 
   llenarData(){
@@ -29,6 +31,12 @@ export class PaquetesTuristicosComponent {
   llenarData2(){
     this.apiService.getDataVarios("3,4").subscribe( data => {
       this.data2= data;
+    })
+  }
+
+  llenarDataSofia(){
+    this.apiService.getDataVarios("5,6").subscribe( data => {
+      this.dataSofia= data;
     })
   }
 
