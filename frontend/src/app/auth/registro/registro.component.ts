2f60a236b733e85467adf82c35d9b1e9c769f229
Nicolás Ruiz <98530951+NicolasRuiz81@users.noreply.github.com>
2023-05-26 16:17:10 -0300

    Formularios con validaciones login y registro
    Envio formularios de registro y login con todas las validaciones hechas.

--- frontend/src/app/auth/registro/registro.component.ts
+++ frontend/src/app/auth/registro/registro.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+  registroForm=this.formBuilder.group({
+    name:['', Validators.required],
+    surname:['', Validators.required],
+    date:['', Validators.required],
+    email:['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  })
+  
+  constructor(private formBuilder:FormBuilder, private router:Router) { }
+  
+  ngOnInit(): void {
+     
+  }
+
+  get name(){
+    return this.registroForm.controls.name;
+  }
+
+  get surname(){
+    return this.registroForm.controls.surname;
+  }
+
+  get date(){
+    return this.registroForm.controls.date;
+  }
+
+  get email(){
+    return this.registroForm.controls.email;
+  }
+
+  get password(){
+    return this.registroForm.controls.password;
+  }
+
+  registro() {
+    if(this.registroForm.valid){
+      console.log("ingresar");
+      this.router.navigateByUrl('/home');
+      this.registroForm.reset();
+    }
+    else{
+      this.registroForm.markAllAsTouched();
+      alert("No se puede ingresar");
+    }
+
+
+  }
 
 }
+
