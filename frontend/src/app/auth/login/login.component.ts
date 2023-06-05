47db34996e394b84cd53d9de5bd7980d26815d3f
Pablo Ballatore <pfb1983@outlook.com>
2023-06-04 22:31:01 -0300

    conexion a la base de datos

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,36 +9,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm=this.formBuilder.group({
-    email:['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-  })
   
-  constructor(private formBuilder:FormBuilder, private router:Router) { }
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+  constructor(private formBuilder:FormBuilder, private router:Router, private ApiService:ApiService) { }
   
   ngOnInit(): void {
-     
+    this.loginForm=this.formBuilder.group({
+      email:['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
   }
 
   get email(){
-    return this.loginForm.controls.email;
+    return this.loginForm.controls['email'];
   }
 
   get password(){
-    return this.loginForm.controls.password;
+    return this.loginForm.controls['password'];
   }
 
   login() {
-    if(this.loginForm.valid){
-      console.log("ingresar");
-      this.router.navigateByUrl('/home');
-      this.loginForm.reset();
-    }
-    else{
-      this.loginForm.markAllAsTouched();
-      alert("No se puede ingresar");
-    }
-
+    // if(this.loginForm.valid){
+    //   console.log("ingresar");
+    //   this.router.navigateByUrl('/home');
+    //   this.loginForm.reset();
+    // }
+    // else{
+    //   this.loginForm.markAllAsTouched();
+    //   alert("No se puede ingresar");
+    // }
+    const valores = this.loginForm.value
+    this.ApiService.login(valores.email, valores.password).subscribe(res => {
+      let error: boolean = false
+      console.log(res.usuario)
+      if (res.message == "success") {
+        alert("Se ha logueado correctamente")
+        // this.router.navigateByUrl('/login');
+        // this.loginForm.reset();
+      }
+      else {
+        this.loginForm.markAllAsTouched();
+        alert(res.message);
+      }
+    })
 
   }
 
--- frontend/src/app/service/api.service.ts
+++ frontend/src/app/service/api.service.ts
@@ -7,18 +7,37 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-
   private urlApi = 'https://rickandmortyapi.com/api/character/1,183';
   private urlApiVarios = 'https://rickandmortyapi.com/api/character/';
 
   constructor(private http: HttpClient) { }
 
-public getData(): Observable<any> {
-return this.http.get<any>(this.urlApi);
+  public getData(): Observable<any> {
+    return this.http.get<any>(this.urlApi);
 
-}
-
-public getDataVarios(ids:string): Observable<any> {
-  return this.http.get<any>(this.urlApiVarios+ids);
   }
+
+  public getDataVarios(ids: string): Observable<any> {
+    return this.http.get<any>(this.urlApiVarios + ids);
+  }
+
+  public createUsuario(Nombre:string, Apellido: string, FechaNacimiento: Date, CorreoElectronico: string, 
+    password: string): Observable<any> {
+    const datos = {
+      Nombre: Nombre, Apellido: Apellido, FechaNacimiento: FechaNacimiento, 
+      CorreoElectronico: CorreoElectronico, admin: 0, password:password, login: 0
+    }
+    return this.http.post<any>('http://localhost:8000/api/usuario/register/', datos);
+  }
+
+  public login(Email:string, password: string): Observable<any>{
+    const datos = {
+      CorreoElectronico: Email,
+      password: password
+    }
+    return this.http.post<any>('http://localhost:8000/api/usuario/login/', datos);
+  } 
 }
+
+
+
